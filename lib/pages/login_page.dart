fe62a821907675baa260cd82c5699b7edb00b801
taiteng <taitengchan@gmail.com>
2023-08-04 12:40:12 +0800

    done login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cryphive/pages/forgot_password_page.dart';
+import 'package:cryphive/pages/home_page.dart';
+import 'package:cryphive/widgets/button_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,46 +14,53 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
-  final _formKey = GlobalKey<FormState>();
+  bool checkedValue = false;
+  List textFieldsStrings = [
+    "", //email
+    "", //password
+  ];
+
+  final _emailKey = GlobalKey<FormState>();
+  final _passwordKey = GlobalKey<FormState>();
 
   void signIn() async{
-    // try{
-    //   await FirebaseAuth.instance.signInWithEmailAndPassword(
-    //     email: emailController.text,
-    //     password: passwordController.text,
-    //   );
-    //
-    //   Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HiddenDrawer(pageNum: 0,)));
-    // } on FirebaseAuthException catch (e) {
-    //   if(e.code == 'user-not-found'){
-    //     showDialog(
-    //       context: context,
-    //       builder: (context) {
-    //         return const AlertDialog(
-    //           backgroundColor: Colors.pinkAccent,
-    //           title: Text('Incorrect Email'),
-    //         );
-    //       },
-    //     );
-    //   }
-    //   else if(e.code == 'wrong-password'){
-    //     showDialog(
-    //       context: context,
-    //       builder: (context) {
-    //         return const AlertDialog(
-    //           backgroundColor: Colors.pinkAccent,
-    //           title: Text('Incorrect Password'),
-    //         );
-    //       },
-    //     );
-    //   }
-    // }
-    await FirebaseFirestore.instance.collection('Users').doc('Test').set({
-      'Email' : 'test',
-      'Phone' : 'test',
-      'Username' : 'test',
-      'ProfilePic' : 'https://media.istockphoto.com/id/1316420668/vector/user-icon-human-person-symbol-social-profile-icon-avatar-login-sign-web-user-symbol.jpg?s=612x612&w=0&k=20&c=AhqW2ssX8EeI2IYFm6-ASQ7rfeBWfrFFV4E87SaFhJE=',
-    });
+    try{
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: textFieldsStrings[0],
+        password: textFieldsStrings[1],
+      );
+
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomePage()));
+    } on FirebaseAuthException catch (e) {
+      if(e.code == 'user-not-found'){
+        showDialog(
+          context: context,
+          builder: (context) {
+            return const AlertDialog(
+              backgroundColor: Colors.pinkAccent,
+              title: Text('Incorrect Email'),
+            );
+          },
+        );
+      }
+      else if(e.code == 'wrong-password'){
+        showDialog(
+          context: context,
+          builder: (context) {
+            return const AlertDialog(
+              backgroundColor: Colors.pinkAccent,
+              title: Text('Incorrect Password'),
+            );
+          },
+        );
+      }
+    }
+    // await FirebaseFirestore.instance.collection('Users').doc('Test').set({
+    //   'Email' : 'test',
+    //   'Phone' : 'test',
+    //   'Username' : 'test',
+    //   'ProfilePic' : 'https://media.istockphoto.com/id/1316420668/vector/user-icon-human-person-symbol-social-profile-icon-avatar-login-sign-web-user-symbol.jpg?s=612x612&w=0&k=20&c=AhqW2ssX8EeI2IYFm6-ASQ7rfeBWfrFFV4E87SaFhJE=',
+    // });
   }
 
   @override
@@ -59,13 +70,288 @@ class _LoginPageState extends State<LoginPage> {
 
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // if (_formKey.currentState!.validate()) {
-            //   signIn();
-            // }
-            signIn();
-          }, child: null,
+        child: Container(
+          height: size.height,
+          width: size.height,
+          decoration: BoxDecoration(
+            color: const Color(0xff151f2c),
+          ),
+          child: SafeArea(
+            child: Stack(
+              children: [
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: size.height * 0.02),
+                        child: Align(
+                          child: Text(
+                            'Hey there,',
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: size.height * 0.02,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: size.height * 0.015),
+                        child: Align(
+                          child: Text(
+                            'Welcome Back',
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: size.height * 0.025,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: size.height * 0.01),
+                      ),
+                      Form(
+                        child: buildTextField(
+                          "Email",
+                          Icons.email_outlined,
+                          false,
+                          size,
+                              (valuemail) {
+                            if (valuemail.length < 5) {
+                              buildSnackError(
+                                'Invalid email',
+                                context,
+                                size,
+                              );
+                              return '';
+                            }
+                            if (!RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+.[a-zA-Z]+")
+                                .hasMatch(valuemail)) {
+                              buildSnackError(
+                                'Invalid email',
+                                context,
+                                size,
+                              );
+                              return '';
+                            }
+                            return null;
+                          },
+                          _emailKey,
+                          0,
+                        ),
+                      ),
+                      Form(
+                        child: buildTextField(
+                          "Passsword",
+                          Icons.lock_outline,
+                          true,
+                          size,
+                              (valuepassword) {
+                            if (valuepassword.length < 6) {
+                              buildSnackError(
+                                'Invalid password',
+                                context,
+                                size,
+                              );
+                              return '';
+                            }
+                            return null;
+                          },
+                          _passwordKey,
+                          1,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: size.width * 0.015,
+                          vertical: size.height * 0.025,
+                        ),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                  const ForgotPasswordPage()),
+                            );
+                          },
+                          child: Text(
+                            "Forgot your password?",
+                            style: TextStyle(
+                              color: const Color(0xffADA4A5),
+                              decoration: TextDecoration.underline,
+                              fontSize: size.height * 0.02,
+                            ),
+                          ),
+                        ),
+                      ),
+                      AnimatedPadding(
+                        duration: const Duration(milliseconds: 500),
+                        padding: EdgeInsets.only(top: size.height * 0.085),
+                        child: ButtonWidget(
+                          text: "Login",
+                          backColor: [
+                            Colors.black,
+                            Colors.black,
+                          ],
+                          textColor: const [
+                            Colors.white,
+                            Colors.white,
+                          ],
+                          onPressed: () async {
+                            //validation for login
+                            if (_emailKey.currentState!.validate()) {
+                              if (_passwordKey.currentState!.validate()) {
+                                signIn();
+                              }
+                            }
+                          },
+                        ),
+                      ),
+                      AnimatedPadding(
+                        duration: const Duration(milliseconds: 500),
+                        padding: EdgeInsets.only(
+                          top: size.height * 0.15,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'CRYPHIVE',
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontSize: size.height * 0.045,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Image.asset('assets/images/cryphive_logo_nobg.png', height: 40,),
+                          ],
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+
+                        },
+                        child: RichText(
+                          textAlign: TextAlign.left,
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "Don’t have an account yet? ",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: size.height * 0.018,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  bool pwVisible = false;
+  Widget buildTextField(
+      String hintText,
+      IconData icon,
+      bool password,
+      size,
+      FormFieldValidator validator,
+      Key key,
+      int stringToEdit,
+      ) {
+    return Padding(
+      padding: EdgeInsets.only(top: size.height * 0.025),
+      child: Container(
+        width: size.width * 0.9,
+        height: size.height * 0.05,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: const BorderRadius.all(Radius.circular(15)),
+        ),
+        child: Form(
+          key: key,
+          child: TextFormField(
+            style: TextStyle(
+                color: const Color(0xffADA4A5)
+            ),
+            onChanged: (value) {
+              setState(() {
+                textFieldsStrings[stringToEdit] = value;
+              });
+            },
+            validator: validator,
+            textInputAction: TextInputAction.next,
+            obscureText: password ? !pwVisible : false,
+            decoration: InputDecoration(
+              errorStyle: const TextStyle(height: 0),
+              hintStyle: const TextStyle(
+                color: Color(0xffADA4A5),
+              ),
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(
+                top: size.height * 0.012,
+              ),
+              hintText: hintText,
+              prefixIcon: Padding(
+                padding: EdgeInsets.only(
+                  top: size.height * 0.005,
+                ),
+                child: Icon(
+                  icon,
+                  color: const Color(0xff7B6F72),
+                ),
+              ),
+              suffixIcon: password
+                  ? Padding(
+                padding: EdgeInsets.only(
+                  top: size.height * 0.005,
+                ),
+                child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      pwVisible = !pwVisible;
+                    });
+                  },
+                  child: pwVisible
+                      ? const Icon(
+                    Icons.visibility_off_outlined,
+                    color: Color(0xff7B6F72),
+                  )
+                      : const Icon(
+                    Icons.visibility_outlined,
+                    color: Color(0xff7B6F72),
+                  ),
+                ),
+              )
+                  : null,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  ScaffoldFeatureController<SnackBar, SnackBarClosedReason> buildSnackError(
+      String error, context, size) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 2),
+        backgroundColor: Colors.black,
+        content: SizedBox(
+          height: size.height * 0.02,
+          child: Center(
+            child: Text(error),
+          ),
         ),
       ),
     );
