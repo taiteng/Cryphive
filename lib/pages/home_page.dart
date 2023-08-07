f36627749f24b83b70394b57b81ae8d52bee2c3d
taiteng <taitengchan@gmail.com>
2023-08-07 12:20:51 +0800

    done login, forgot password, register page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:cryphive/pages/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,10 +11,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  void signOut() async{
+    await FirebaseAuth.instance.signOut();
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage(),),);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home'),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            signOut();
+          },
+          child: Text(
+            'Logout'
+          ),
+        ),
+      ),
     );
   }
 }
